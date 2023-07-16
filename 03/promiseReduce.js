686bd01a259d9da9487e90fc6421381c61833cd8
Anton Karmanov <anton@karmanov.ws>
2023-07-16 18:02:26 +0700

    add 03: promiseReduce

---
+++ 03/promiseReduce.js
@@ -0,0 +1,15 @@
+function promiseReduce(asyncFunctions, reduce, initialValue) {
+    let functionResult = Promise.resolve(initialValue);
+
+    for (let asyncFunction of asyncFunctions) {
+        functionResult = functionResult.then(
+            promiseResult => asyncFunction().then(
+                value => reduce(promiseResult, value)
+            )
+        );
+    }
+
+    return functionResult;
+}
+
+module.exports = promiseReduce;
---
+++ 03/promiseReduce.test.js
@@ -0,0 +1,33 @@
+const promiseReduce = require('./promiseReduce');
+
+const asyncFunctions = [
+    () => {
+        console.log('fn1');
+        return Promise.resolve(1);
+    },
+    () => new Promise(resolve => {
+        console.log('fn2');
+        setTimeout(() => resolve(2), 1000);
+    }),
+];
+
+const reduce = (memo, value) => {
+    console.log('reduce');
+    return memo * value;
+};
+
+describe('promiseReduce', () => {
+    test('should resolve with the correct value', async () => {
+        const result = await promiseReduce(asyncFunctions, reduce, 1);
+        expect(result).toBe(2);
+    });
+
+    test('should reject if any of the promises is rejected', async () => {
+        const failingAsyncFunctions = [
+            ...asyncFunctions,
+            () => Promise.reject(new Error('test error')),
+        ];
+
+        await expect(promiseReduce(failingAsyncFunctions, reduce, 1)).rejects.toThrow('test error');
+    });
+})
